1cf1790bbbe88762e658b5d9c9f912cd743db987
BlWoAlf <blwoalf@inbox.ru>
2021-04-12 11:45:10 +0700

    12.04 laravel mix bootstrap link

--- resources/views/pattern.blade.php
+++ resources/views/pattern.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @yield('title')
     <link rel="shortcut icon" href="{{asset('images/apartments-2.png')}}" type="image/x-icon">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/tools.css')}}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
